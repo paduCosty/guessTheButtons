3f5739b9bd1540c6141ce72f892ce15f2605582c
paduCosty <78753049+paduCosty@users.noreply.github.com>
2021-02-11 16:15:47 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,28 +1,26 @@
-let steop = 1, lungime = 0;
-const caca = [];
+let stop = 1, inputLenght = 0;
+const arrayConst = [];
 const buttonsContainer = [];
 function buttonFunction() {
     let nrButton = document.getElementById("txtInput").value;
-    lungime = nrButton;
-   // document.write(nrButton);
-   for (var i = 1; i <= nrButton && steop == 1; i++) {
+    inputLenght = nrButton;
+   for (var i = 1; i <= nrButton && stop == 1; i++) {
         buttonsContainer[i] = document.getElementById("text1");
-        caca[i] = document.createElement("button");
-        caca[i].innerText = "Select the button";
-        buttonsContainer[i].appendChild(caca[i]); 
+        arrayConst[i] = document.createElement("button");
+        arrayConst[i].innerText = "Select the button";
+        buttonsContainer[i].appendChild(arrayConst[i]); 
     }
-    steop =0 ;
+    stop =0 ;
     rnd = Math.floor(Math.random(1) * nrButton);
     rnd += 1;
     buttonsContainer[rnd] = rnd;
-   // document.write(rnd);
 }
 let x = 0;
 function alegeNr() {
-    for(let i = 0; i <= lungime; ++i) {   
+    for(let i = 0; i <= inputLenght; ++i) {   
         if(buttonsContainer[i] == rnd && x == 0) {
-            caca[i].innerText = "Acesta este numarul corect";
-            document.getElementById("cartof").innerHTML += rnd;
+            arrayConst[i].innerText = "Acesta este numarul corect";
+            document.getElementById("correctNumber").innerHTML += rnd;
             x = 1;
         }
     }
